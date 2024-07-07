f43837e2ad907e5b4dc54ff5142e124ec4436cae
Rabiya-Abubakar <Rabiya.abubakar1@student.moringaschool.com>
2024-07-07 15:08:01 +0300

    Update index.js

---
+++ extra_bubblesort/index.js
@@ -0,0 +1,19 @@
+// JS Solution
+
+function bubbleSort(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+// Sample usage
+const inputArray = [5, 6, 1, 3, 4, 2];
+console.log("Sorted Array:", bubbleSort(inputArray));
