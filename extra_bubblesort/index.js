1f68edeeb290e71fababe88880b4ddf12173b6c4
Rabiya-Abubakar <Rabiya.abubakar1@student.moringaschool.com>
2024-07-07 15:58:46 +0300

    Update index.js
    REPOSITORY ASSIGNMENT

--- extra_bubblesort/index.js
+++ extra_bubblesort/index.js
@@ -1,18 +1,20 @@
 // Solution.js
 function bubbleSort(arr) {
-    let len = arr.length;
-    for (let i = 0; i < len; i++) {
-        for (let j = 0; j < len - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {
-                let temp = arr[j];
+    let len = arr.length; // Get the length of the array
+    for (let i = 0; i < len; i++) { // Outer loop for each pass
+        for (let j = 0; j < len - 1 - i; j++) { // Inner loop for comparisons
+            if (arr[j] > arr[j + 1]) { // Compare adjacent elements
+                let temp = arr[j]; // Swap if current element is greater than next element
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
     }
-    return arr;
+    return arr; // Return the sorted array
 }
 
 // Sample usage
 const inputArray = [5, 6, 1, 3, 4, 2];
-console.log("Sorted Array:", bubbleSort(inputArray));
+console.log("Sorted Array:", bubbleSort(inputArray)); // Output the sorted array
+
+
