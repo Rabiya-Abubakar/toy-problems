39ac99b1698f1c9c5ce9f2c7528790bac1f3bad0
Rabiya-Abubakar <Rabiya.abubakar1@student.moringaschool.com>
2024-07-07 16:06:55 +0300

    Update index.js

--- grading_assignment/index.js
+++ grading_assignment/index.js
@@ -1,8 +1,12 @@
 //Solution.js
 function getStudentGrade() {
+  // Loop indefinitely until valid input is received
   while (true) {
+    // Prompt the user to enter a student mark and convert it to a number
     const mark = parseFloat(prompt("Enter student mark (between 0 and 100): "));
+     // Check if the input is a valid number within the range of 0 to 100
     if (!isNaN(mark) && mark >= 0 && mark <= 100) {
+      // Determine and print the grade based on the mark
       if (mark > 79) {
         console.log("Student grade: A");
         return "Grade A";
@@ -20,9 +24,10 @@ function getStudentGrade() {
         return "Grade E";
       }
     } else {
+      // Inform the user of invalid input and continue the loop
       console.log("Invalid input. Please enter a mark between 0 and 100.");
     }
   }
 }
-
+// Invoke the function to start grading & prints the grade
 getStudentGrade();
